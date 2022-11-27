5be2e151876ddaeb31d8e29c26a80bf961717381
FauL <deadmaksimchik@gmail.com>
2022-11-27 13:06:11 +0300

    add assertions on login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,7 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage, LoginPageLocators):
     def should_be_login_page(self):
-        self.should_be_login_url(self.url, 'login')
+        self.should_be_login_url(self.browser.current_url, 'login')
         self.should_be_login_form()
         self.should_be_register_form()
 
